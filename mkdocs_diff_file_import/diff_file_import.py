7313d4f4e9aae9034822db4e0842dcc493ca2504
Francesco Lombardo <francesco.lombardo@garr.it>
2024-09-04 17:07:00 +0200

    fix: changed import style

--- mkdocs_diff_file_import/diff_file_import.py
+++ mkdocs_diff_file_import/diff_file_import.py
@@ -46,12 +46,11 @@ class ImportDiff(mkdocs.plugins.BasePlugin[GenericImportDiffConfig]):
 
     def _load_file_to_import(self, file_path) -> str:
         if os.path.isfile(file_path):
-            file_content = open(file_path).read()
-            #result_code_block = f"```diff \n\n{file_content} \n\n```"
             result_code_block = f'''
-            ```diff
-                {file_content}
-            ```'''
+``` diff
+--8<-- \"{file_path}\"
+```\n
+'''
         else:
             result_code_block = f"```diff file not found. cwd {str(os.listdir(os.getcwd()))}```"
         return result_code_block
